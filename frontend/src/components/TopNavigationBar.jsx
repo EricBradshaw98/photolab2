1f29ec347b2d8c8e9da5b875e7ca386bc7b62941
Eric <ericbradshaw98@gmail.com>
2024-02-21 14:30:53 -0500

    added notification

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,15 @@ import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = (props) => {
+
+  const isFavPhotoExist = props.favouritePhotos.length > 0;
+
   return (
     <div className="top-nav-bar">
       
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={props.topics}/>
-      <FavBadge setFavouritePhotos={props.setFavouritePhotos}favouritePhotos={props.favouritePhotos} />
+      <FavBadge setFavouritePhotos={props.setFavouritePhotos}favouritePhotos={props.favouritePhotos} isFavPhotoExist={isFavPhotoExist} />
 
     </div>
   )
