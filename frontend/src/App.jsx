0172b5d0d3ab36a9cb610cb0f554c0208eb582f8
Eric <ericbradshaw98@gmail.com>
2024-02-20 21:35:23 -0500

    modified with Larry suggestion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,9 +25,7 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      
-        <PhotoList />
+      <HomeRoute />
         
       
     </div>
